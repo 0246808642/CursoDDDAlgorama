b59acc91178490a55fa5dde895119aee8b4c8a4c
Caique Coelho Piheiro <caiquepinheiro69@gmail.com>
2025-10-07 11:50:47 -0300

    Criação das entidades, e utilizando o Kernel

---
+++ Churras/Empresa.Churras.Domain.Model/Entities/Evento.cs
@@ -0,0 +1,13 @@
+﻿using Kernel.Domain.Model.Entities;
+
+namespace Empresa.Churras.Domain.Model.Entities;
+
+public class Evento : EntityKeySeq
+{
+    public long DonoDaCasaKey { get; set; }
+    public Colega? DonoDaCasa { get; set; }
+    public string? TipoEvento { get; set; }
+    public DateTime Dia { get; set; }
+    public string? Periodo { get; set; }
+    public IList<EventoColegaConfirmado>? ColegasQueConfirmaramPresenca { get; set; }
+}
